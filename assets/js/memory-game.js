9d4fafc829dc7607e2f8ef8328ba261b0121ae06
Selina Erhabor <selinaerhabor@gmail.com>
2018-10-20 23:49:58 +0000

    Inserted the audio files that will be used as part of the game’s sound features: blue buzzer, green buzzer, red buzzer, yellow buzzer, incorrect, correct notification sounds and opening theme song for the game. Functions have been created for each sound of the buzzers

---
+++ assets/js/memory-game.js
@@ -0,0 +1,23 @@
+/*------------------------------------------------------GREEN BUTTON*/
+
+  var Green = new Audio();
+      Green.src = "assets/sounds/noteE.wav";
+      
+  
+/*------------------------------------------------------YELLOW BUTTON*/
+     
+   var Yellow = new Audio();
+      Yellow.src = "assets/sounds/noteD.wav";
+  
+  
+/*------------------------------------------------------RED BUTTON*/
+  
+  var Red = new Audio();
+      Red.src = "assets/sounds/noteF.wav";
+      
+      
+/*------------------------------------------------------BLUE BUTTON*/
+  
+   var Blue = new Audio();
+      Blue.src = "assets/sounds/noteC.wav";
+      
