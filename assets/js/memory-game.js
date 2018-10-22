d8a86042ea2934cdbe8d7d6f5791e1263bf628ef
Selina Erhabor <selinaerhabor@gmail.com>
2018-10-22 18:39:01 +0000

    Created functions for the buzzer lights in JavaScript. These functions contain the settings for the light effect displayed when the user presses each of the buzzers.

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -3,21 +3,35 @@
   var Green = new Audio();
       Green.src = "assets/sounds/noteE.wav";
       
+   function greenLight(div){
+     div.style.backgroundColor = "#13ff7c";
+  }
+      
   
 /*------------------------------------------------------YELLOW BUTTON*/
      
    var Yellow = new Audio();
       Yellow.src = "assets/sounds/noteD.wav";
   
+  function yellowLight(div){
+     div.style.backgroundColor = "#ffe54c";
+  }
   
 /*------------------------------------------------------RED BUTTON*/
   
   var Red = new Audio();
       Red.src = "assets/sounds/noteF.wav";
-      
+   
+   function redLight(div){
+     div.style.backgroundColor = "#ff6666";
+  }
       
 /*------------------------------------------------------BLUE BUTTON*/
   
    var Blue = new Audio();
       Blue.src = "assets/sounds/noteC.wav";
+      
+   function blueLight(div){
+     div.style.backgroundColor = "#3288ff";
+  }
       
