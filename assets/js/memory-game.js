e94c550523e66116469d84279c733f13383e8220
Selina Erhabor <selinaerhabor@gmail.com>
2018-10-23 19:43:18 +0000

    Start button function has been set. The start button now turns on the game when pressed as the screen display changes from -- (sleep mode) to 00 (on mode)

--- assets/js/memory-game.js
+++ assets/js/memory-game.js
@@ -1,61 +1,67 @@
 
 /*------------------------------------------------------GREEN BUTTON*/
 
-  var Green = new Audio();
+    var Green = new Audio();
       Green.src = "assets/sounds/noteE.wav";
       
-   function greenLight(div){
-     div.style.backgroundColor = "#13ff7c";
+    function greenLight(div){
+      div.style.backgroundColor = "#13ff7c";
   }
   
-  function greenInactive(div){
-     div.style.backgroundColor = "#00a74a";
+    function greenInactive(div){
+      div.style.backgroundColor = "#00a74a";
   }
       
 /*------------------------------------------------------YELLOW BUTTON*/
      
-   var Yellow = new Audio();
+    var Yellow = new Audio();
       Yellow.src = "assets/sounds/noteD.wav";
   
-  function yellowLight(div){
-     div.style.backgroundColor = "#ffe54c";
+    function yellowLight(div){
+      div.style.backgroundColor = "#ffe54c";
   }
   
-  function yellowInactive(div){
-     div.style.backgroundColor = "#cca707";
+    function yellowInactive(div){
+      div.style.backgroundColor = "#cca707";
   }
   
 /*------------------------------------------------------RED BUTTON*/
   
-  var Red = new Audio();
+    var Red = new Audio();
       Red.src = "assets/sounds/noteF.wav";
    
-   function redLight(div){
-     div.style.backgroundColor = "#ff6666";
+    function redLight(div){
+      div.style.backgroundColor = "#ff6666";
   }
   
-  function redInactive(div){
-     div.style.backgroundColor = "#9f0f17";
+    function redInactive(div){
+      div.style.backgroundColor = "#9f0f17";
   }
       
 /*------------------------------------------------------BLUE BUTTON*/
   
-   var Blue = new Audio();
+    var Blue = new Audio();
       Blue.src = "assets/sounds/noteC.wav";
       
-   function blueLight(div){
-     div.style.backgroundColor = "#3288ff";
+    function blueLight(div){
+      div.style.backgroundColor = "#3288ff";
   }
   
-  function blueInactive(div){
-     div.style.backgroundColor = "#094a8f";
+    function blueInactive(div){
+      div.style.backgroundColor = "#094a8f";
   }
       
 /*------------------------------------------------------THEME TUNE*/
 
-var mimicStartThemeTune = new Audio();
-mimicStartThemeTune.src = "assets/sounds/mimicStartThemeTune.wav";
+    var mimicStartThemeTune = new Audio();
+    mimicStartThemeTune.src = "assets/sounds/mimicStartThemeTune.wav";
 
 
-/*------------------------------------------------------*/
-
+/*------------------------------------------------------START BUTTON*/
+    $(document).ready(function(){
+        $("#start-btn").click(function(){
+            $(".count").text("00");
+            level = 0;
+            level++;
+        });
+    });
