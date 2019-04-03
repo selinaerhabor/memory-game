82e02765ec653c980d0056ac46074106f9637f90
Selina Erhabor <selinaerhabor@gmail.com>
2019-04-03 08:46:49 +0000

    Test folders added

---
+++ Tests/jasmine-tests/spec/mimicSpec.js
@@ -0,0 +1,18 @@
+describe("Mimic Game function", function() {
+    
+    var id = [];
+    
+    //Start Button
+    
+    
+    
+    //Game Initiator
+    
+
+    
+    //Buzzer Selection
+    describe("Selects an ID number at random - 0, 1, 2 or 3", function() {
+        expect(0).toHaveBeenCalled();
+    });
+
+})
